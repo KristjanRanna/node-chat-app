51d8799941a545cfb193c27222e139864bcbe391
Kristjan Ranna <kristjanranna@gmail.com>
2018-04-15 10:59:55 +0300

    Greet new and alert others

--- server/server.js
+++ server/server.js
@@ -18,15 +18,29 @@ io.on('connection', (socket) => {
   //   subject: "Some email title here",
   //   name: 'Kristjan'
   // });
-  
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app!'
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined.'
+  });
+
 
   socket.on('createMessage', function(message) {
     console.log('createMessage', message);
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    // io.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
