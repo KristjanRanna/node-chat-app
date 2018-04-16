9175a18509399c5423c2b2e8aa23679306471c63
Kristjan Ranna <kristjanranna@gmail.com>
2018-04-16 09:20:19 +0300

    Add Moment.js

---
+++ playground/time.js
@@ -0,0 +1,8 @@
+var moment = require('moment');
+
+// var date = moment();
+// date.add(1, 'year');
+// console.log(date.format('MMM Do, YYYY'));
+
+var date = moment();
+console.log(date.format('H:mm a'));
